05cd1a695f163dc459aa617607e12713d4327d44
wooyoung0407 <wooyoung0407@naver.com>
2023-08-01 12:02:41 +0900

    3차

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './App.css'
 import TodoList from './components/TodoList'
 import TodoInsert from './components/Todoinsert'
@@ -8,18 +8,42 @@ import TodoInsert from './components/Todoinsert'
 function App() {
 
   const [todos, setTodos] = useState([
-    { id: 1, text: "공부", checked: true },
-    { id: 2, text: "운동", checked: true },
-    { id: 3, text: "명상", checked: false },
+    {
+      id: 1,
+      text: "공부",
+      checked: true,
+    },
+    {
+      id: 2,
+      text: "운동",
+      checked: true,
+    },
+    {
+      id: 3,
+      text: "명상",
+      checked: false,
+    },
   ]);
 
+  const nextId = useRef(4);
+
+  const onInsert = (text) => {
+    setTodos(
+      todos.concat([{
+        id: nextId.current,
+        text: text,
+        checked: false,
+      }])
+    );
+    nextId.current++;
+  };
 
   return (
     <div>
-      <TodoInsert />
-      <TodoList todos={todos}/>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} />
     </div>
-  )
+  );
 }
 
 export default App
--- src/components/TodoInsert.jsx
+++ src/components/TodoInsert.jsx
@@ -1,13 +1,30 @@
-function TodoInsert() {
-  return ( 
-    <form>
-      <input 
-      type="text" 
-      placeholder="할일을 입력하세요."
+import { useState } from "react";
+
+function TodoInsert({ onInsert }) {
+  const [text, setText] = useState("");
+
+  const onChange = (e) => {
+    console.log(e.target.value);
+    setText(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    onInsert(text);
+    setText("");
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        value={text}
+        placeholder="할일을 입력하세요."
+        onChange={onChange}
       />
       <button type="submit">입력</button>
     </form>
-   );
+  );
 }
 
 export default TodoInsert;
